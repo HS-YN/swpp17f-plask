92cfc5e28fa48fb58b1ed672623a01510b703688
wogur0602 <chlwogur0413@snu.ac.kr>
2017-11-02 17:49:28 +0900

    migration done, test for signin

--- PlaskBack/user/models.py
+++ PlaskBack/user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class UserInfo(models.Model):
-	is_active = models.BooleanField()				# Sync with User
+	is_active = models.BooleanField(default = True) # Sync with User
 	location1 = models.CharField(max_length = 100)	# Country
 	location2 = models.CharField(max_length = 100)	# State
 	location3 = models.CharField(max_length = 100)	# City
--- PlaskBack/user/tests.py
+++ PlaskBack/user/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import UserInfo
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+import json
 
-# Create your tests here.
+class UserTestCase(TestCase):
+	def setUp(self):
+		self.client = Client()
+
+		client = Client(enforce_csrf_checks = True)
+		response = client.get('/api/user/token')
+		csrftoken = response.cookies['csrftoken'].value
+		self.client.post(
+			'/api/user/signup',
+			json.dumps({'username': 'PlaskTest1',
+				'password': '123123',
+				'location1': 'South Korea',
+				'location2': 'Seoul',
+				'location3': 'Kwanak-gu'}),
+			content_type = 'application/json',
+			HTTP_X_CSRFTOKEN = csrftoken)
+		
+	def test_signin_ok(self):
+		response = self.client.post('/api/user/signin', json.dumps({'username': 'PlaskTest1', 'password': '123123'}), content_type = 'application.json')
+		self.assertEqual(response.status_code, 204)
