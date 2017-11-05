85fd508beba08a489bc5f8c973935b195809f1ae
Park Jiyeon <timedilation@snu.ac.kr>
2017-11-05 15:23:24 +0900

    add location test in user/test.py

--- PlaskBack/user/tests.py
+++ PlaskBack/user/tests.py
@@ -7,28 +7,39 @@ from django.contrib.auth.models import User
 import json
 
 class UserTestCase(TestCase):
-	def setUp(self):
-		self.client = Client()
-		getLocationFromCSVFile ('./location/korea.CSV')
+  def setUp(self):
+    self.client = Client()
+    getLocationFromCSVFile ('./location/korea.CSV')
 
-		client = Client(enforce_csrf_checks = True)
-		response = client.get('/api/user/token')
-		csrftoken = response.cookies['csrftoken'].value
-		response = self.client.post(
-			'/api/user/signup',
-			json.dumps({
-				'email': 'PlaskTest1@snu.ac.kr',
-				'username': 'PlaskTest1',
-				'password': '123123',
-				'locations': 'South Korea/Busan/Buk;South Korea/Busan/Busanjin',
-				'services': 'coffee;pizza;'
-				}),
-			content_type = 'application/json',
-			HTTP_X_CSRFTOKEN = csrftoken)
-	
-	def test_signin_ok(self):
-		response = self.client.post('/api/user/signin', json.dumps({'username': 'PlaskTest1@snu.ac.kr', 'password': '123123'}), content_type = 'application.json')
-		self.assertEqual(response.status_code, 204)
+    client = Client(enforce_csrf_checks = True)
+    response = client.get('/api/user/token')
+    csrftoken = response.cookies['csrftoken'].value
+    response = self.client.post(
+        '/api/user/signup',
+        json.dumps({
+          'email': 'PlaskTest1@snu.ac.kr',
+          'username': 'PlaskTest1',
+          'password': '123123',
+          'locations': 'South Korea/Busan/Buk;South Korea/Busan/Busanjin',
+          'services': 'coffee;pizza;'
+          }),
+        content_type = 'application/json',
+        HTTP_X_CSRFTOKEN = csrftoken)
 
-	def test_signin_failed(self):
-		response = self.client.post('/api/user/signin', json.dumps({'username': 'PlaskTest1', 'password': '123123'}), content_type = 'application.json')
+  def test_signin_ok(self):
+    response = self.client.post('/api/user/signin', json.dumps({'username': 'PlaskTest1@snu.ac.kr', 'password': '123123'}), content_type = 'application.json')
+    self.assertEqual(response.status_code, 204)
+
+  def test_signin_failed(self):
+    response = self.client.post('/api/user/signin', json.dumps({'username': 'PlaskTest1', 'password': '123123'}), content_type = 'application.json')
+
+  def test_get_loc_list(self):
+    response = self.client.get('/api/location/countries', content_type = 'application.json')
+    data = json.loads(response.content.decode())
+    print(data)
+    response = self.client.get('/api/location/South Korea', content_type = 'application.json')
+    data = json.loads(response.content.decode())
+    print(data)
+    response = self.client.get('/api/location/South Korea/Seoul', content_type = 'application.json')
+    data = json.loads(response.content.decode())
+    print(data)
