8b4e242435bd4fa20aebae8928b66395af64a246
Park Jiyeon <timedilation@snu.ac.kr>
2017-11-05 15:16:56 +0900

    made get location list

--- PlaskBack/location/views.py
+++ PlaskBack/location/views.py
@@ -7,3 +7,41 @@ import json
 # MIGHT BE USED / do not check test
 # get location code by name
 
+def getCountries(request):
+    if request.method == 'GET': 
+        cons = list(LocationL1.objects.all().values())
+        result = ''
+        for con in cons:
+          result = result+con['name']
+          result = result + ';'
+        return JsonResponse(result, safe=False)
+    else:
+        return HttpResponseNotAllowed(['POST','PUT','DELETE'])
+
+def getLv1Child(request, loc_lv1):
+    if request.method == 'GET':
+        loc = LocationL1.objects.get(name = loc_lv1)
+        locs = list(loc.child.all().values())
+        result = ''
+        for temploc in locs:
+          result = result+temploc['name']
+          result = result+';'
+        return JsonResponse(result, safe=False)
+    else:
+        return HttpResponseNotAllowed(['POST','PUT','DELETE'])
+
+
+def getLv2Child(request, loc_lv1, loc_lv2):
+    if request.method == 'GET':
+        loc = LocationL1.objects.get(name = loc_lv1)
+        loc2 = loc.child.get(name = loc_lv2)
+        locs = list(loc2.child.all().values())
+        result = ''
+        for temploc in locs:
+          result = result+temploc['name']
+          result = result+';'
+        return JsonResponse(result, safe=False)
+    else:
+        return HttpResponseNotAllowed(['POST','PUT','DELETE'])
+
+
