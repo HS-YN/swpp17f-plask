b6f93f17b4468d0feb721d026b5f776d1b895307
wogur0602 <chlwogur0413@snu.ac.kr>
2017-12-03 14:11:51 +0900

    add getting related question & modify response data order

--- PlaskBack/ask/views.py
+++ PlaskBack/ask/views.py
@@ -22,8 +22,6 @@ def question_to_dict(question):
     result['locations'] = getLocationStr(question)
     result['services'] = getServiceStr(question)
     return result
-
-
 def answer_to_dict(answer):
     result = {}
     result['id'] = answer.id
@@ -32,14 +30,67 @@ def answer_to_dict(answer):
     result['author'] = answer.author.nickname
     return result
 
+def getQuestionByLocCode (loc_code_set):
+    try:
+        l1 = LocationL1.objects.get(loc_code = loc_code_set.loc_code1)
+    except LocationL1.DoesNotExist:
+        raise Question.DoesNotExist
+    if loc_code_set.loc_code2 >= 0:
+        try:
+            l2 = l1.child.get(loc_code = loc_code_set.loc_code2)
+        except LocationL2.DoesNotExist:
+            raise Question.DoesNotExist
+    else:
+        l2 = None
+    if loc_code_set.loc_code2 >= 0 and loc_code_set.loc_code3 >= 0:
+        try:
+            l3 = l2.child.get(loc_code = loc_code_set.loc_code3)
+        except LocationL3.DoesNotExist:
+            raise Question.DoesNotExist
+    else:
+        l3 = None
+
+    TIME_DIFF = 7 # 1 week
+    start_time = datetime.now().date() - timedelta(TIME_DIFF)
+    if l3 is not None:
+        return list(l3.questions.order_by('time').filter(time__gte = start_time).all())
+    elif l2 is not None:
+        return list(l2.questions.order_by('time').filter(time__gte = start_time).all())
+    else:
+        return list(l1.questions.order_by('time').filter(time__gte = start_time).all())
+def getServiceMatchPoint (question, services):
+    match_count = 0
+    for service in services.all():
+        for q_service in question.services.all():
+            if service.name == q_service.name:
+                match_count = match_count + 1
+    return match_count
+def sortQuestionByService (questions, services):
+    result = []
+    for question in questions:
+        result.append ((getServiceMatchPoint(question, services), question))
+    return [point_question[1] for point_question in sorted(result, key = lambda point_question: point_question[0], reverse = True)]
+def isMyQuestion (question, userinfo):
+    for myQuestion in userinfo.questions.all():
+        if question.id == myQuestion.id:
+            return True
+    return False
+def filterQuestion (questions, blocked, userinfo):
+    result = []
+    for question in questions:
+        if getServiceMatchPoint(question, blocked) == 0 and not isMyQuestion(question, userinfo):
+            result.append (question)
+    return result
+
 
 @login_required
 def question(request):
     if request.method == 'GET':
         author = request.user.userinfo
+        questions = list(author.questions.order_by('time').all())
+        questions.reverse()
         return JsonResponse(
-            # TODO order_by
-            [question_to_dict (question) for question in author.questions.all()],
+            [question_to_dict (question) for question in questions],
             safe=False)
     elif request.method == 'POST':
         author = request.user.userinfo
@@ -51,7 +102,10 @@ def question(request):
             author=author, content=content, time=datetime.now())
         new_question.save()
         setService(new_question, services)
-        setLocation(new_question, locations)
+        try :
+            setLocation(new_question, locations)
+        except Question.DoesNotExist:
+            return HttpResponse(status=400)
         return HttpResponse(status=204)
     else:
         return HttpResponseNotAllowed(['GET', 'POST'])
@@ -62,36 +116,37 @@ def question_recent(request):
     if request.method == 'GET':
         yesterday = datetime.now().date() - timedelta(1)
         start_time = datetime.combine(yesterday, time())
+        questions = list(Question.objects.order_by('time').filter(time__gte=start_time))
+        questions.reverse()
         return JsonResponse(
-            [question_to_dict(question) for question in Question.objects.order_by('time').filter(time__gte=start_time)],
+            [question_to_dict(question) for question in questions],
             safe=False)
     else:
         return HttpResponseNotAllowed(['GET'])
 
-'''
 @login_required
 def question_related(request):
     if request.method == 'GET':
-        curr_user = request.user.userinfo
-        location = curr_user.locations.all().values()[0]
-        tag_search = curr_user.services.all().values()
-        relevant_questions = list(Question.objects.filter(locations__loc_code1=location['loc_code1']).filter(
-            locations__loc_code2=location['loc_code2']
-        ).filter(locations__loc_code3=location['loc_code3']).values())
-        selected_questions = []
-        for question_given in relevant_questions:
-            weight = 0
-            threshold = len(question_given.services) * 0.7
-            # TODO: for question_given, there is an attributeError: "dict object has no attribute objects"
-            for question_tag in question_given.services:
-                if tag_search.find(question_tag) != -1:
-                    weight = weight + 1
-            if weight >= threshold:
-                selected_questions.append(question_given)
-        return selected_questions
+        userinfo = request.user.userinfo
+        locations = userinfo.locations.all()
+        services = userinfo.services
+        blocked = userinfo.blocked
+        
+        MAX_COUNT = 30
+        result = []
+        for location in locations:
+            try:
+                result = result + getQuestionByLocCode(location)
+            except Question.DoesNotExist:
+                return HttpResponse (status = 400) # invalid location code
+        result.reverse()
+        result = sortQuestionByService (result, services)
+        result = filterQuestion (result, blocked, userinfo)
+        return JsonResponse(
+            [question_to_dict(question) for question in result[:MAX_COUNT]],
+            safe = False)
     else:
         return HttpResponseNotAllowed(['GET'])
-'''
 
 '''
 # TODO: There is a same problem with question_related.
@@ -132,7 +187,9 @@ def question_answer(request):
     if request.method == 'GET':
         author = request.user.userinfo
         questions = []
-        for answer in author.answers.all():
+        answers = list(author.answers.order_by('time').all())
+        answers.reverse()
+        for answer in answers:
             if answer.question not in questions:
                 questions.append(answer.question)
         return JsonResponse(
@@ -150,8 +207,10 @@ def answer(request, question_id):
         return HttpResponseNotFound()
 
     if request.method == 'GET':
+        answers = list(curr_question.answers.all())
+        answers.reverse()
         return JsonResponse(
-            [answer_to_dict (answer) for answer in curr_question.answers.all()], safe=False)
+            [answer_to_dict (answer) for answer in answers], safe=False)
     elif request.method == 'POST':
         author = request.user.userinfo
         req_body = json.loads(request.body.decode())
@@ -162,25 +221,6 @@ def answer(request, question_id):
         return HttpResponse(status=204)
     else:
         return HttpResponseNotAllowed(['GET', 'POST'])
-
-'''
-def service2index(service_list):
-    if len(service_list) <= 0:
-        return []
-    index_list = []
-    for services in service_list:
-        index_list.append(Service.objects.get(name=services).id)
-    return index_list
-def location2index(location_list):
-    index_list = [-1, -1, -1]
-    if len(location_list) >= 1 & len(location_list[0]) > 0:
-        index_list[0] = LocationL1.objects.get(name=location_list[0].replace("%20", " ")).id
-    if len(location_list) >= 2 & len(location_list[1]) > 0:
-        index_list[1] = LocationL2.objects.get(name=location_list[1].replace("%20", " ")).id
-    if len(location_list) >= 3 & len(location_list[2] > 0):
-        index_list[1] = LocationL2.objects.get(name=location_list[2].replace("%20", " ")).id
-    return index_list;
-'''
 
 def setLocation(question, location_list):
     question.locations.clear()
