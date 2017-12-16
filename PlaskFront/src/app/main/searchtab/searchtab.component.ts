c4a25b37262a9ea34a7b097d388ce47f90ddb69a
Park Jiyeon <timedilation@snu.ac.kr>
2017-12-16 21:27:13 +0900

    load answers when question expanded

--- PlaskFront/src/app/main/searchtab/searchtab.component.ts
+++ PlaskFront/src/app/main/searchtab/searchtab.component.ts
@@ -42,53 +42,69 @@ export class SearchTabComponent implements OnInit{
             this.searchString = params['str'];
             this.locCode = [params['id1'], params['id2'], params['id3']];
         });
+        this.getSearchQuestionList(this.searchString, this.locCode);
 
-        this.questionService.getSearchedQuestion(this.searchString, this.locCode).then(
+    /*    this.questionService.getSearchedQuestion(this.searchString, this.locCode).then(
             questionList => {
                 this.temp_questionList = questionList;
                 this.getAnswerList();
             })
         console.log(this.temp_questionList);
-        this.userService.getUser().then(User => {this.user = User});
+      */  this.userService.getUser().then(User => {this.user = User});
+    }
+
+    getQuestionList():void {
+        this.questionList = [];
+        this.questionService.getQuestion(1).then(questions =>{
+            for(let q of questions){
+                this.questionList.push([q, true, []]);
+            }
+        });
+    }
+    getAnswer(qid:number, qindex:number){
+        this.answerService.getAnswer(qid).then(answers => {
+            this.questionList[qindex][2] = answers;
+        });
     }
 
     getSearchQuestionList(searchString: string, locCode: string[]):void {
-            this.questionList = [];
-            this.questionService.getSearchedQuestion(searchString, locCode).then(questions => {
-                this.temp_questionList = questions;
-                this.getAnswerList();
-            });
+        this.questionList = [];
+        this.questionService.getSearchedQuestion(searchString, locCode).then(questions => {
+            if(questions.length === 0){
+                alert("No question matched!");
+            }
+            else{
+                for(let q of questions){
+                    this.questionList.push([q, true, []]);
+                }
+                alert("Search Complete!");
+            }
+        });
     }
-
-    getAnswerList():void{
-        if(this.temp_questionList.length === 0){
-            alert("No question matched!");
-            return;
-        }
-        alert("Search Complete!");
-        for(let q of this.temp_questionList){
-            var temp_answerList = [];
-            this.answerService.getAnswer(q.id).then(answers =>{
-                if(answers != null)
-                    temp_answerList = answers;
-                    this.questionList.push([q, true, temp_answerList]);
-                    console.log(this.questionList)});
-        }
-    };
 
     expand(question):void {
         // expand if hidden
         if(question[1]==true){
+            let qindex:number;
 
             // hide all other questions that are expanded
             for (let i =0; i< this.questionList.length; ++i){
                 if (this.questionList[i][1] == false){
                     this.questionList[i][1] =true;
                 }
+                //get where the question located in list
+                if(this.questionList[i][0].id == question[0].id){
+                    qindex=i;
+                }
             }
             this.answer = ""; //clear answer tab
             question[1] = false;
             this.chooseAnswerEnable = (question[0].author === this.user.username) && (question[0].select_id === -1);
+
+            //get answers if it is not loaded
+            if(question[2].length == 0){
+                question[2] = this.getAnswer(question[0].id, qindex);
+            }
         }
         // collapse if opened
         else{
