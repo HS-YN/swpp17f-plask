704e95213e0ecac1f20381cde8e2a40e1f48c9a4
Seong Hoon Seo <andyseo247@gmail.com>
2017-12-18 21:06:21 +0900

    Added some pages

---
+++ PlaskFront/src/app/miscellaneous/blank.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'blank',
+    templateUrl: './blank.component.html',
+    //styleUrls: [ './blank.component.css']
+})
+export class BlankComponent implements OnInit {
+
+    ngOnInit(){
+
+    }
+
+ } /* istanbul ignore next */
---
+++ PlaskFront/src/app/miscellaneous/pagenotfound.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'pagenotfound',
+    templateUrl: './pagenotfound.component.html',
+    //styleUrls: [ './blank.component.css']
+})
+export class PageNotFoundComponent implements OnInit {
+
+    ngOnInit(){
+        
+    }
+
+ } /* istanbul ignore next */
