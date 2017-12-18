3d570136dbce7abbd29a612f8fa37496aaee6571
wogur0602 <chlwogur0413@snu.ac.kr>
2017-12-18 18:27:59 +0900

    update travis test env(11)

--- PlaskBack/PlaskBack/settings.py
+++ PlaskBack/PlaskBack/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import pymysql
+# import pymysql
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'PlaskBack.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-pymysql.install_as_MySQLdb ()
+# pymysql.install_as_MySQLdb ()
 
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
