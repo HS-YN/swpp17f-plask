26ab378d05e10de502c9ad464b6fec36a95a3df7
Seong Hoon Seo <andyseo247@gmail.com>
2017-12-18 21:08:37 +0900

    fixed a typo

--- PlaskFront/src/app/miscellaneous/pagenotfound.component.ts
+++ PlaskFront/src/app/miscellaneous/pagenotfound.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'pagenotfound',
     templateUrl: './pagenotfound.component.html',
-    //styleUrls: [ './blank.component.css']
+    //styleUrls: [ './pagenotfound.component.css']
 })
 export class PageNotFoundComponent implements OnInit {
 
