804cdbfc538f45a9406f35cac43be38da6ebb5da
wogur0602 <chlwogur0413@snu.ac.kr>
2017-12-18 16:48:29 +0900

    set db as mysql

--- PlaskBack/PlaskBack/settings.py
+++ PlaskBack/PlaskBack/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import pymysql
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,11 +80,19 @@ WSGI_APPLICATION = 'PlaskBack.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+pymysql.install_as_MySQLdb ()
+
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'blog',
+		'USER': 'root',
+		'PASSWORD': 'plask',
+		'HOST': '127.0.0.1',
+		'PORT': '3306'
     }
 }
 
