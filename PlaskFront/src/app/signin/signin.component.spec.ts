03d9d96a69fcf02c4237ce2c16c1f32c9f623d82
Seong Hoon Seo <andyseo247@gmail.com>
2017-12-18 22:50:54 +0900

    Enhance FrontendTesting: SignIn

--- PlaskFront/src/app/signin/signin.component.spec.ts
+++ PlaskFront/src/app/signin/signin.component.spec.ts
@@ -1,77 +1,113 @@
-// import { TestBed, ComponentFixture, async, fakeAsync, tick } from '@angular/core/testing';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { DebugElement } from '@angular/core';
-// import { By } from '@angular/platform-browser';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
+import { AppModule } from '../app.module';
+import { SignInComponent } from './signin.component';
+import { UserService } from '../user/user.service';
+import { User } from '../user/user';
 
-// import { SignInComponent } from './signin.component';
-// import { AppModule } from '../app.module';
+let comp: SignInComponent;
+let fixture: ComponentFixture<SignInComponent>;
 
-// let comp: SignInComponent;
-// let fixture: ComponentFixture<SignInComponent>;
+describe('SignInComponent', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [AppModule, RouterTestingModule.withRoutes([]) ]
+        }).overrideModule(AppModule, {
+            remove: {
+                providers: [
+                    UserService
+                ]
+            },
+            add: {
+                providers: [
+                    { provide: UserService, useClass: FakeUserService}
+                ]
+            }
+        }).compileComponents().then(() => {
+            fixture = TestBed.createComponent(SignInComponent);
+            comp = fixture.componentInstance;
+            
+            fixture.detectChanges();
+            fixture.whenStable().then(() => {
+                fixture.detectChanges();
+            })
+        });
+    }));
 
-// describe('SignInComponent', () => {
-//     beforeEach(async(() => {
-//         TestBed.configureTestingModule({
-//             imports: [AppModule, RouterTestingModule.withRoutes([]) ]
-//         }).compileComponents().then(() => {
-//             fixture = TestBed.createComponent(SignInComponent);
-//             comp = fixture.componentInstance;
-//             fixture.detectChanges();
-//             fixture.whenStable().then(() => {
-//                 fixture.detectChanges();
-//             })
-//         });
-//     }));
+    it('can be instantiated', () => {
+        expect(comp).not.toBeNull();
+        expect(comp).toBeTruthy();
+    });
 
-//     it('can be instantiated', () => {
-//         expect(comp).not.toBeNull();
-//         expect(comp).toBeTruthy();
-//     });
+    it('should have "Plask!" as title,', async(() => {
+        fixture.whenStable().then(() => {
+            expect(comp.title).toEqual('Plask!');
+        });
+    }));
 
-//     it('can be initialized', () => {
-//         expect(comp.ngOnInit).toThrow();
-//         expect(comp.SignIn).toThrow();
-//         expect(comp.goToMain).toThrow();
-//         expect(comp.goToSignUp).toThrow();
-//     })
+    it('should have "Location-based live Q&A platform" as subtitle', async(() => {
+        fixture.whenStable().then(() => {
+        expect(comp.subtitle).toEqual('Location-based live Q&A platform');
+        });
+    }));
 
-//     it('should have as title Plask!,', async(() => {
-//         fixture.whenStable().then(() => {
-//             expect(comp.title).toEqual('Plask!');
-//         })
-//     }))
+    it('should have a user variable instantiated', async(() => {
+        fixture.whenStable().then(() => {
+            expect(comp.user).not.toBeNull();
+        });
+    }));
 
-//     it('should have as subtitle Location-based live Q&A platform', async(() => {
-//         fixture.whenStable().then(() => {
-//         expect(comp.subtitle).toEqual('Location-based live Q&A platform');
-//         })
-//     }))
+    it('should successfully signin a proper user', async(() => {
+        let spy = spyOn(comp, "goToMain");
+        comp.user.email = 'swpp@gmail.com';
+        comp.user.password = 'iluvswpp';
+        comp.SignIn();
 
-//     it('should have user notificaiton frequency set to 1 as default', () => {
-//         expect(comp.user.notiFrequency).toEqual(1);
-//     });
+        fixture.whenStable().then(() => {
+            expect(spy).toHaveBeenCalled();
+        });
+    }));
 
+    it('should not login a improper user', async(() => {
+        let spy = spyOn(window, "alert");
+        comp.user.email = 'wrong@email.com';
+        comp.user.password = 'wrongpassword';
+        comp.SignIn();
 
-//     it('should have Email: as the first label', async(() => {
-//         let label = fixture.debugElement.queryAll(By.css('label')).map(de => de.nativeElement);
-//         expect(label[0]).not.toBeNull();
-//     }))
+        fixture.whenStable().then(() => {
+            expect(spy).toHaveBeenCalledWith("Signin failed. Please try again!");
+        });
+    }));
 
-//     // Added in Sprint 3
-//     it ('should have display correct labels for input', async(() => {
-//         const labels = fixture.debugElement.queryAll(By.css('label'));
-//         const emailLabel = labels[0].nativeElement;
-//         const passwordLabel = labels[1].nativeElement;
+    it('can navigate to main', async(() => {
+        let navigateSpy = spyOn((<any>comp).router, 'navigate');
+        comp.goToMain();
+        expect(navigateSpy).toHaveBeenCalledWith(['/main']);
+    }));
 
-//         expect(emailLabel.classList.contains("Email:")).toBeTruthy();
-//         expect(passwordLabel.classList.contains("Password:")).toBeTruthy();
-//     }))
+    it ('can navigate to signup', async(() =>{
+        let navigateSpy = spyOn((<any>comp).router, 'navigate');
+        comp.goToSignUp();
+        expect(navigateSpy).toHaveBeenCalledWith(['/signup']);
+    }));
 
-//     it ('can navigate to signup', async(() =>{
-//         let navigateSpy = spyOn((<any>comp).router, 'navigate');
-//         comp.goToSignUp();
-//         expect(navigateSpy).toHaveBeenCalledWith(['/signup']);
-//     }))
+});
 
-// });
+export const fakeUsers: User[] = [
+    {email: 'swpp@gmail.com', password: 'iluvswpp', username: 'swpplover', locations: 'Korea/Seoul/Gwanak;', services: 'SNU;Cafe;', blockedServices: ';', notiFrequency: 10},
+    {email: 'test1@gmail.com', password: 'test1', username: 'testuser1', locations: 'Korea/Seoul;', services: 'Music;Guitar;', blockedServices: ';', notiFrequency: 10},
+];
+
+class FakeUserService {
+  signIn(user: User): Promise<number>{
+      for(let i = 0; i < fakeUsers.length; ++i){
+          if ((user.email == fakeUsers[i].email) && (user.password == fakeUsers[i].password)){
+              return Promise.resolve<number>(204);
+          }
+      }
+      return Promise.resolve<number>(403);
+  }
+}
