2af26bd7c2252016d3f11d1128df8a372dbbaf4d
wogur0602 <chlwogur0413@snu.ac.kr>
2017-12-19 04:03:33 +0900

    find way to copy data from sqlite to mysql

--- PlaskBack/PlaskBack/settings.py
+++ PlaskBack/PlaskBack/settings.py
@@ -84,16 +84,16 @@ WSGI_APPLICATION = 'PlaskBack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
-#		'ENGINE': 'django.db.backends.mysql',
-#		'NAME': 'blog',
-#		'USER': 'root',
-#		'PASSWORD': 'plask',
-#		'HOST': '127.0.0.1',
-#		'PORT': '3306'
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'plask',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+    },
 }
 
 # default in-memory channel layer
